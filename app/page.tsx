30a304361db6ef495d3f057edd07b2614b93ec9d
Owner <ghifariezraramadhan@gmail.com>
2025-06-25 19:23:04 +0700

    ADD:section contact

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroContent from "./components/Hero/HeroContent";
 import ServiceContent from "./components/Service/ServiceContent";
+import ContactContent from "./components/Contact/ContactDetail";
 
 export default function Home() {
   return (
     <main className="home-hero">
       <HeroContent />
       <ServiceContent />
+      <ContactContent />
     </main>
   );
 }
