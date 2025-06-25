95a14e32d398e6f2b0d5c4313a8d884b59771966
Owner <ghifariezraramadhan@gmail.com>
2025-06-25 15:24:23 +0700

    ADD:service section

---
+++ app/components/Service/ServiceContent.tsx
@@ -0,0 +1,10 @@
+export default function ServiceContent() {
+  return (
+    <section className="wrapper-hero bg-gray-100">
+      <div className="flex flex-col gap-4 justify-center items-center text-center">
+        <h1 className="font-bold text-2xl sm:text-3xl md:text-5xl">Our Services</h1>
+        <p className="text-gray-600 text-base sm:text-lg md:text-xl w-full">We offer a variety of services to cater to your needs, including meal planning, catering, and personalized nutrition advice.</p>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroContent from "./components/Hero/HeroContent";
+import ServiceContent from "./components/Service/ServiceContent";
 
 export default function Home() {
   return (
     <main className="home-hero">
       <HeroContent />
+      <ServiceContent />
     </main>
   );
 }
