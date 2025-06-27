ef2cd7a140a73139f908523348289a130ce5ed7d
Owner <ghifariezraramadhan@gmail.com>
2025-06-27 17:52:44 +0700

    ADD : USER API

---
+++ app/api/(rest)/user/route.ts
@@ -0,0 +1,35 @@
+// app/api/me/route.ts
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export async function GET() {
+    const cookieStore = cookies();
+    const token = (await cookieStore).get("token")?.value;
+
+    if (!token) {
+        return NextResponse.json({ isLoggedIn: false }, { status: 200 });
+    }
+
+    try {
+        const { payload } = await jwtVerify(token, secret);
+
+        return NextResponse.json(
+            {
+                isLoggedIn: true,
+                user: {
+                    id: payload.id,
+                    email: payload.email,
+                    full_name: payload.full_name,
+                    role: payload.role, // jika kamu include role
+                },
+            },
+            { status: 200 }
+        );
+    } catch (err) {
+        console.error("[JWT_VERIFY_ERROR]", err);
+        return NextResponse.json({ isLoggedIn: false }, { status: 200 });
+    }
+}
