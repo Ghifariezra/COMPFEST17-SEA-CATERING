eaf799ab5bf87c23192fc11e9e03d2f05b92df67
Owner <ghifariezraramadhan@gmail.com>
2025-06-27 10:30:49 +0700

    ADD:add menu section

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,10 @@
 export default function MenuContent() {
     return (
-        <div>
-            <h1>Menu Page</h1>
+      <section className="wrapper-hero bg-white">
+        <div className="wrapper-service">
+          <h1 className="heading-service">What Our Customers Say</h1>
+          <p className="description-service">Join thousands of satisfied customers who have transformed their lives with SEA Catering&apos;s healthy meal plans. Real stories from real people across Indonesia.</p>
         </div>
+      </section>
     );
-}
+} 
